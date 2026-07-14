35349cd3cbbe6478b07c18c6db192763c725c204
Akki7171 <oneforallmail1@gmail.com>
2026-07-14 12:48:39 +0530

    Initial commit

---
+++ components/InputText.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const InputText = () => {
+  return (
+    <View >
+      <Text>InputText</Text>
+    </View>
+  );
+};
+
+export default InputText;
+
+const styles = StyleSheet.create({});
